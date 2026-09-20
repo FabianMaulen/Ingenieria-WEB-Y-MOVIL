490056511c3d94bb846d83837cb64c2ff878595c
Fabian Alejandro  Maulen Gonzalez <fabianmaulen4@gmail.com>
2026-09-20 15:37:37 -0300

    correccion de choque de versiones

--- PROYECTOBAUL/src/pages/Login.tsx
+++ PROYECTOBAUL/src/pages/Login.tsx
@@ -15,10 +15,10 @@ import {
   IonButton,
   IonText
 } from '@ionic/react';
-import { useNavigate } from 'react-router-dom';
+import { useHistory  } from 'react-router-dom';
 
 export const Login: React.FC = () => {
-const navigate = useNavigate();
+const history = useHistory ();
   const [identifier, setIdentifier] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
@@ -39,7 +39,7 @@ const navigate = useNavigate();
     setErrorMessage('');
     // Simulación de autenticación exitosa
     localStorage.setItem('auth_token', 'session_activa_token');
-    navigate('/dashboard', { replace: true });
+    history('/dashboard', { replace: true });
   };
 
   return (
